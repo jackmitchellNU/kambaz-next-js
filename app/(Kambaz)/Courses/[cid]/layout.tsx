1d1f85766322cde850988c880bbcc38ca45acd80
Jack Mitchell <mitchell.jack@northeastern.edu>
2025-09-19 15:21:14 -0600

    test

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,19 +1,25 @@
-import { ReactNode } from "react";
-import CourseNavigation from "./Navigation";
-export default async function CoursesLayout(
-  { children, params }: Readonly<{ children: ReactNode; params: Promise<{ id: string }> }>) {
- const { cid } = await params;
- return (
-   <div id="wd-courses">
-     <h2>Courses {cid}</h2>
-     <hr />
-     <table>
-       <tbody>
-         <tr>
-           <td valign="top" width="200"> <CourseNavigation /> </td>
-           <td valign="top" width="100%"> {children} </td>
-         </tr>
-       </tbody>
-     </table>
-   </div>
-);}
+import CourseNavigation from './Navigation';
+
+interface CoursesLayoutProps {
+  children: React.ReactNode;
+  params: { cid: string };
+}
+
+export default function CoursesLayout({ children, params }: CoursesLayoutProps) {
+  const { cid } = params;
+
+  return (
+    <div id="wd-courses">
+      <h2>Courses {cid}</h2>
+      <hr />
+      <table>
+        <tbody>
+          <tr>
+            <td valign="top" width="200"><CourseNavigation /></td>
+            <td valign="top" width="100%">{children}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
