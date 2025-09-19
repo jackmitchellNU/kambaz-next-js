ad7dba91951000711528f75d1b60df4513dad02f
Jack Mitchell <mitchell.jack@northeastern.edu>
2025-09-19 15:57:40 -0600

    more

--- app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
@@ -15,7 +15,39 @@ export default function AssignmentEditor() {
               <input id="wd-points" defaultValue={100} />
             </td>
           </tr>
-          {/* Complete on your own */}
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-due-date">Due Date</label>
+            </td>
+            <td>
+              <input id="wd-due-date" type="date" />
+            </td>
+          </tr>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-available-date">Available From</label>
+            </td>
+            <td>
+              <input id="wd-available-date" type="date" />
+            </td>
+          </tr>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-assignment-type">Assignment Type</label>
+            </td>
+            <td>
+              <select id="wd-assignment-type" defaultValue="Homework">
+                <option value="Homework">Homework</option>
+                <option value="Quiz">Quiz</option>
+                <option value="Project">Project</option>
+                <option value="Exam">Exam</option>
+              </select>
+            </td>
+            <td>
+                <button type="button">Save</button>
+                <button type="button">Cancel</button>
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
