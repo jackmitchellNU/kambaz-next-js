426ab92f49fd662dd8b6de419cc98e06b5a62e51
Jack Mitchell <mitchell.jack@northeastern.edu>
2025-10-27 22:25:15 -0400

    Frst commit

---
+++ app/Labs/Lab4/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import PassingFunctions from "./PassingFunctions";
+export default function Lab4() {
+  function sayHello() {
+    alert("Hello");
+  }
+  return (
+    <div id="wd-passing-functions">
+      <h2>Lab 4</h2>
+      ...
+      <PassingFunctions theFunction={sayHello} />
+    </div>
+);}
