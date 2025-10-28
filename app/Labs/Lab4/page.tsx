5b7b2f45b77a608ad41916574fee9521b0db506a
jackmitchellNU <jackmitchellcol@gmail.com>
2025-10-27 23:00:51 -0400

    Up to 4.3

--- app/Labs/Lab4/page.tsx
+++ app/Labs/Lab4/page.tsx
@@ -1,4 +1,11 @@
 "use client"
+import ArrayStateVariable from "./ArrayStateVariable";
+import ChildStateComponent from "./ChildStateComponent";
+import ClickEvent from "./ClickEvent";
+import EventObject from "./EventObject";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
 export default function Lab4() {
   function sayHello() {
@@ -9,5 +16,13 @@ export default function Lab4() {
       <h2>Lab 4</h2>
       ...
       <PassingFunctions theFunction={sayHello} />
+      <EventObject />
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <ArrayStateVariable />
+      <ObjectStateVariable />
+      <ParentStateComponent />
+
+      
     </div>
 );}
