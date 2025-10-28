ef6da805e47aa1562242940265ba6342ec526708
Jack Mitchell <jmitchell@corp.exergen.com>
2025-10-28 12:26:33 -0400

    4.3.5.1

--- app/Labs/Lab4/page.tsx
+++ app/Labs/Lab4/page.tsx
@@ -1,20 +1,27 @@
 "use client"
 import ArrayStateVariable from "./ArrayStateVariable";
 import ChildStateComponent from "./ChildStateComponent";
+import ReduxExamples from "./ReduxExamples/page";
 import ClickEvent from "./ClickEvent";
 import EventObject from "./EventObject";
 import ObjectStateVariable from "./ObjectStateVariable";
 import ParentStateComponent from "./ParentStateComponent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
+import store from "./Store/page";
+import { Provider } from "react-redux";
+import TodoList from "./ReduxExamples/todos/todoList";
+import CounterRedux from "./ReduxExamples/CounterRedux/page";
+import HelloRedux from "./ReduxExamples/HelloRedux/page";
+import AddRedux from "./ReduxExamples/AddRedux/page";
 export default function Lab4() {
   function sayHello() {
     alert("Hello");
   }
   return (
+    <Provider store={store}>
     <div id="wd-passing-functions">
       <h2>Lab 4</h2>
-      ...
       <PassingFunctions theFunction={sayHello} />
       <EventObject />
       <ClickEvent />
@@ -22,7 +29,11 @@ export default function Lab4() {
       <ArrayStateVariable />
       <ObjectStateVariable />
       <ParentStateComponent />
-
-      
+      <ReduxExamples />
+      <AddRedux />
+      <CounterRedux />
+      <HelloRedux />
+      <TodoList />
     </div>
+    </Provider>
 );}
