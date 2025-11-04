3fe32acd60b6184f3a9442ae4228bcc3a0fcd321
Jack Mitchell <jmitchell@corp.exergen.com>
2025-11-03 19:55:13 -0500

    Test for add reducer

--- app/(Kambaz)/store.ts
+++ app/(Kambaz)/store.ts
@@ -2,9 +2,21 @@ import { configureStore } from "@reduxjs/toolkit";
 import coursesReducer from "./Courses/reducer";
 import modulesReducer from "./Courses/[cid]/Modules/reducer";
 import accountReducer from "./Account/reducer";
+import helloReducer from "../Labs/Lab4/ReduxExamples/HelloRedux/helloReducer";
+import counterReducer from "../Labs/Lab4/ReduxExamples/CounterRedux/CounterReducer";
+import addReducer from "../Labs/Lab4/ReduxExamples/AddRedux/addReducer";
+import todosReducer from "../Labs/Lab4/ReduxExamples/todos/todosReducer";
 
 const store = configureStore({
- reducer: { coursesReducer, modulesReducer, accountReducer },
+ reducer: { 
+   coursesReducer, 
+   modulesReducer, 
+   accountReducer,
+   helloReducer,
+   counterReducer,
+   addReducer,
+   todosReducer,
+ },
 });
 export type RootState = ReturnType<typeof store.getState>;
 export default store;
