c7abf023df436490cacfeecb915a3efa5bc991e1
Jack Mitchell <jmitchell@corp.exergen.com>
2025-11-24 23:59:55 -0500

    p217

--- app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
+++ app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
@@ -1,19 +1,15 @@
 "use client"
 import { useParams } from "next/navigation";
-import * as db from "../../../../Database";
 import { FaUserCircle } from "react-icons/fa";
+
 
 type Enrollment = { _id: string; user: string; course: string };
 
-export default function PeopleTable() {
-  const { cid } = useParams<{ cid: string }>();
-  const enrollments = db.users as unknown as Enrollment[];
-  const courseEnrollments = enrollments.filter((e) => e.course === cid);
-
+export default function PeopleTable({ users = [], fetchUsers }: { users?: any[]; fetchUsers: () => void; }) {
   return (
     <div id="wd-people-table" className="container-fluid p-3">
-      <h3 className="mb-3">People ({courseEnrollments.length})</h3>
-      {courseEnrollments.length === 0 ? (
+      <h3 className="mb-3">People ({users.length})</h3>
+      {users.length === 0 ? (
         <div className="text-muted">No people enrolled in this course.</div>
       ) : (
         <div className="table-responsive">
@@ -25,12 +21,12 @@ export default function PeopleTable() {
               </tr>
             </thead>
             <tbody>
-              {courseEnrollments.map((enr) => (
-                <tr key={enr._id}>
+              {users.map((user) => (
+                <tr key={user._id}>
                   <td>
                     <FaUserCircle className="me-2 fs-3 text-secondary" />
                   </td>
-                  <td className="text-nowrap">{enr.user}</td>
+                  <td className="text-nowrap">{user}</td>
                 </tr>
               ))}
             </tbody>
